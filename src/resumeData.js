2afe0c40496457ebded91b61b0f48c8995f88da8
M1koto <yenming891109@gmail.com>
2021-10-25 00:26:37 -0700

    personal web

---
+++ src/resumeData.js
@@ -0,0 +1,78 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/M1koto.github.io",
+    "name": "Kenny Liao",
+    "roleDescription": "I study computer science at University of California, Berkeley. My career interests include: Software Engineering, Machine learning, Data Science, and Computer Security. I am Currently looking for internship in Summer 2022!",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/kenny-liao-46b55b198/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/M1koto",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am currently a junior at the University of California, Berkeley and pursuing a Bachelor degree in Computer Science and minoring in Data Science. Throughout clubs, previous internships, side projects, and courseworks, I have developed a passion for software engineering and machine learning. I hope to pursue a career in software engineering and machine learning to help solve complex problems and build a more efficient world!",
+    "website":"yenming891109@gmail.com",
+    "education":[
+      {
+        "UniversityName":"University of California, Berkeley",
+        "specialization":"Computer Science and Data Science",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2019 - Now",
+        "Relavent Coursework":"Computer Security, Data Structure, Designing Information Devices and Systems II, Machine Structures, Efficient Algorithms and Intractable Problems, Introduction to Database Systems, Structure and Interpretation of Computer Programs, Introduction to Artificial Intelligence"
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"Joinvest",
+        "specialization":"Software Engineering Intern",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2021",
+        "Achievements":"• Designed Requirement and API docs from scratch, implemented and tested code with Python, PostgresQL, for admin and client API of reselling bonds, then deployed using Docker, Ansible, GCP.",
+        "Achievements2": "• Implemented chaincode in NodeJS and ExpressJS with Hyperledger Fabric and wrote scripts to upload the daily 1000+ transaction data to blockchain from PostgresQL in an automated manner."
+      },
+      {
+        "CompanyName":"UC Berkeley Electrical Engineering & Computer Sciences (EECS)",
+        "specialization":"Academic Intern",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2021",
+        "Achievements":"• Answered project questions and conceptual questions that covers C, assembly language, digital design, computer architecture in CS61C office hours.",
+        "Achievements2": "• Assisted discussion sections by answering live questions with GSIs."
+      },
+      {
+        "CompanyName":"RetailingAI",
+        "specialization":"Data Science Intern",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2020",
+        "Achievements":"• Cleaned, fetched, and analyzed over 5 million real world data using Pandas, MySQL, and data models to produce Dashboards in PowerBI in order to help keep track of members, consuming behavior, transactions, and recommendation systems for the client.",
+        "Achievements2":"• Created Classification and Regression models from the provided data to predict purchase behavior, repurchase date, revenue trend, and other important indexes for consulting of clients."
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Sproul Club Recommendation System",
+        "description":"A recommendation system for Sroul.club to recommend clubs to users based on favorited clubs and viewed clubs",
+        "imgurl":"images/portfolio/sather-bear.png"
+      },
+      {
+        "name":"Rookie DB",
+        "description":"A fully functioning DBMS in Java that handles Concurrency and Recovery",
+        "imgurl":"images/portfolio/db.jpeg"
+      },
+      {
+        "name":"Linkedin-Sheet Extension",
+        "description":"A Google Extension that takes a linkedin job page and outputs desired information to user’s google sheet in a click",  
+        "imgurl":"images/portfolio/linkedin_red.png"
+      },
+      {
+        "name":"NUMC",
+        "description":"A C version of the Numpy library, with a Python interface. Achieved speedups: 10X for basic operation, 90X for matrix multiplication, and 1700X for matrix power operation",
+        "imgurl":"images/portfolio/code.jpeg"
+      }
+    ],
+  }
+  
+  export default resumeData
