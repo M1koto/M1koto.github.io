c8c0d4fbad6b1add8c40c0d413d31a752813eb2a
M1koto <yenming891109@gmail.com>
2021-10-25 01:02:23 -0700

    Updates

---
+++ precache-manifest.4326bc76e2d4c198f04896cb6961f08a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "acefdc4f4e87181c301d",
+    "url": "/Kenny_Portfolio/static/css/main.eefb80bb.chunk.css"
+  },
+  {
+    "revision": "acefdc4f4e87181c301d",
+    "url": "/Kenny_Portfolio/static/js/main.acefdc4f.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/Kenny_Portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "930c3fc928a1dd87ed3c",
+    "url": "/Kenny_Portfolio/static/js/runtime~main.930c3fc9.js"
+  },
+  {
+    "revision": "2c28450fbae84a0dc3e53b4a1482d066",
+    "url": "/Kenny_Portfolio/index.html"
+  }
+];
